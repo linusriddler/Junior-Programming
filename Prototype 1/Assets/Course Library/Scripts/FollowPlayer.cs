9dd4a22ff35264331c904d84ed901c9b9d9aadea
Linus Riddle <linusriddler@gmail.com>
2025-09-29 08:39:51 -0400

    9/29/2025
    I changed the speed of the car and added a script to the camera to follow the car.

--- Prototype 1/Assets/Course Library/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 20;
-
+    public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + new Vector3(0,5,-7);
     }
 }
