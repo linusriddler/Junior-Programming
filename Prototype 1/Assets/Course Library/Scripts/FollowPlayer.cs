19bccb6799e9790d0073be54cae26174e296833a
Linus Riddle <linusriddler@gmail.com>
2025-09-30 07:58:27 -0400

    High Speed Chase
    Optimized Code Using Variables. Made the camera follow the car

--- Prototype 1/Assets/Course Library/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,8 +12,8 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0,5,-7);
+        transform.position = player.transform.position + offset;
     }
 }
