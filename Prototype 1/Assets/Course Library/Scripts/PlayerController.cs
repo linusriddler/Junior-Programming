40aa2c5c3c747b93054586971b5a4904940a7a31
Linus Riddle <linusriddler@gmail.com>
2025-09-30 08:24:08 -0400

    Step Into The Drivers Seat
    Added controllable movement and rotation.

--- Prototype 1/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Course Library/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-
+    private float speed = 20.0f;
+    private float turnSpeed = 60.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +15,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Moves the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
