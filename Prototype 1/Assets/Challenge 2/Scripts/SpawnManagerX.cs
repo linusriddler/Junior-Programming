983a465b33c7a9fdc8d90fcfe74f731f879dd41f
Linus Riddle <linusriddler@gmail.com>
2025-10-07 08:40:22 -0400

    Challenge 2
    Fixed Everything (in a really weird way)

--- Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Prototype 1/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
@@ -11,7 +12,7 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = 4f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +25,10 @@ public class SpawnManagerX : MonoBehaviour
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
