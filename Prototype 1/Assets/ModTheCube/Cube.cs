aec883ce280ddd68c8ccd16dff73f92773749822
Linus Riddle <linusriddler@gmail.com>
2025-10-08 07:54:56 -0400

    Exercise - Mod The Cube

---
+++ Prototype 1/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(2, 2, 2);
+        transform.localScale = Vector3.one * 2f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(5f, 10f, 3f, 4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(0f * Time.deltaTime, 1f, 0.0f);
+    }
+}
