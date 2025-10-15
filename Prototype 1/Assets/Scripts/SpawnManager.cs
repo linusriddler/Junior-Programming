0905289fdc942d23de9180e847d9934d6fdd6437
Linus Riddle <linusriddler@gmail.com>
2025-10-15 07:52:28 -0400

    Challenge 3 - Balloons, Bombs, and Booleans

--- Prototype 1/Assets/Scripts/SpawnManager.cs
+++ Prototype 1/Assets/Scripts/SpawnManager.cs
@@ -21,7 +21,7 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnObstacle() 
     {
-        if (playerControllerScript.gameOver == false) 
+        if (playerControllerScript.gameOver) 
         {
             Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation); 
         }
