b876ee76d89278bb54bf40f60648c3763f35dcaf
Linus Riddle <linusriddler@gmail.com>
2025-10-16 07:49:53 -0400

    Lab 3 - Player Control
    I liked making this one.

--- Prototype 1/Assets/Other Scripts/ControlPlayer.cs
+++ Prototype 1/Assets/Other Scripts/ControlPlayer.cs
@@ -15,21 +15,22 @@ public class ControlPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move Up, Down, Left, and Right with W, S, A, and D
         if (Input.GetKeyUp(KeyCode.W) && gameOver == false)
         {
-            playerRb.AddForce(Vector3.forward * velocity, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.forward * velocity, ForceMode.Acceleration);
         }
         if (Input.GetKeyUp(KeyCode.D) && gameOver == false)
         {
-            playerRb.AddForce(Vector3.right * velocity, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.right * velocity, ForceMode.Acceleration);
         }
         if (Input.GetKeyUp(KeyCode.A) && gameOver == false)
         {
-            playerRb.AddForce(Vector3.left * velocity, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.left * velocity, ForceMode.Acceleration);
         }
         if (Input.GetKeyUp(KeyCode.S) && gameOver == false)
         {
-            playerRb.AddForce(Vector3.back * velocity, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.back * velocity, ForceMode.Acceleration);
         }
     }
 }
