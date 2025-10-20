5973b36e96faa0e0fbfa82fba2aaa4792e4db392
Linus Riddle <linusriddler@gmail.com>
2025-10-20 08:41:14 -0400

    10/20/25
    Added 3 Enemies

--- Prototype 1/Assets/Course Library/Scripts/Spawn Manager.cs
+++ Prototype 1/Assets/Course Library/Scripts/Spawn Manager.cs
@@ -7,9 +7,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-   
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+     SpawnEnemyWave(3);
     }
 
     // Update is called once per frame
@@ -25,6 +23,13 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
  
 }
