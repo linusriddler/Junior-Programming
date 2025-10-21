d4cba686296a4e2cbfa71b7cd3ff7e8105425803
Linus Riddle <linusriddler@gmail.com>
2025-10-21 08:41:04 -0400

    10/21/25
    Did most of this challenge

--- Prototype 1/Assets/Course Library/Scripts/Enemy.cs
+++ Prototype 1/Assets/Course Library/Scripts/Enemy.cs
@@ -16,7 +16,7 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed);
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
         if (transform.position.y < -10) 
         {
             Destroy(gameObject);
