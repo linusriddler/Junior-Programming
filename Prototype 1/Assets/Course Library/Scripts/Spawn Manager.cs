cafcda295d9105245bd5b4d4a02439ba5950301d
Linus Riddle <linusriddler@gmail.com>
2025-10-21 07:59:45 -0400

    4.4 - For-Loops For Waves

--- Prototype 1/Assets/Course Library/Scripts/Spawn Manager.cs
+++ Prototype 1/Assets/Course Library/Scripts/Spawn Manager.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-     SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0) 
+        {
+            waveNumber++; SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
     private Vector3 GenerateSpawnPosition()
     {
