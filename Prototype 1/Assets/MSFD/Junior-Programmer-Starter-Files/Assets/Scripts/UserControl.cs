af106d7ba3a99c14a47697b66797018e74a550fa
Linus Riddle <linusriddler@gmail.com>
2025-11-10 08:39:40 -0500

    11/10/25
    Added HandleSelection() and HandleAction() .

--- Prototype 1/Assets/MSFD/Junior-Programmer-Starter-Files/Assets/Scripts/UserControl.cs
+++ Prototype 1/Assets/MSFD/Junior-Programmer-Starter-Files/Assets/Scripts/UserControl.cs
@@ -19,6 +19,27 @@ public class UserControl : MonoBehaviour
     {
         Marker.SetActive(false);
     }
+    public void HandleSelection()
+    {
+        var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //the collider could be children of the unit, so we make sure to check in the parent
+            var unit = hit.collider.GetComponentInParent<Unit>();
+            m_Selected = unit;
+
+
+            //check if the hit object have a IUIInfoContent to display in the UI
+            //if there is none, this will be null, so this will hid the panel if it was displayed
+            var uiInfo = hit.collider.GetComponentInParent<UIMainScene.IUIInfoContent>();
+            UIMainScene.Instance.SetNewInfoContent(uiInfo);
+        }
+    }
+    public void HandleAction()
+    {
+
+    }
 
     private void Update()
     {
@@ -27,20 +48,7 @@ public class UserControl : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            var ray = GameCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                //the collider could be children of the unit, so we make sure to check in the parent
-                var unit = hit.collider.GetComponentInParent<Unit>();
-                m_Selected = unit;
-                
-                
-                //check if the hit object have a IUIInfoContent to display in the UI
-                //if there is none, this will be null, so this will hid the panel if it was displayed
-                var uiInfo = hit.collider.GetComponentInParent<UIMainScene.IUIInfoContent>();
-                UIMainScene.Instance.SetNewInfoContent(uiInfo);
-            }
+
         }
         else if (m_Selected != null && Input.GetMouseButtonDown(1))
         {//right click give order to the unit
